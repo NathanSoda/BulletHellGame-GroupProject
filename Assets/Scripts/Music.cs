79c6b959cb448b703453668e0e4da15383d98429
NathanSoda <nathanwhitehill1999@gmail.com>
2019-11-16 18:52:32 +0000

    Main menu, audio
    Added/fixed the main menus of the game, level select screen etc, still not finshed but will be next push. Added Music to the game, added a music volume slider and made the music persistant between scenes

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour
+{
+	static Music instance = null;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
